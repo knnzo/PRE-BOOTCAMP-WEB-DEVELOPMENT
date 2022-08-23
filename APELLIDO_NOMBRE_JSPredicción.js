cf2c4d6dd9bcee1a2eb938e948ab2efcd65512e9
knnzo <vicente.alcantara.romero@alumnos.sip.cl>
2022-08-23 09:11:21 -0400

    cambio

---
+++ APELLIDO_NOMBRE_JSPredicción.js
@@ -0,0 +1,34 @@
+function myBirthYearFunc(){
+        console.log("Nací en " + 1980);
+    }
+//el console.log imprime por consola naci en 1980
+
+/******************************************************* */
+var EntradaAñoNacimiento=2006;
+
+
+function myBirthYearFunc(EntradaAñoNacimiento){
+            console.log("Nací en " + EntradaAñoNacimiento);
+        }
+
+
+
+/******************************************************* */
+
+
+
+var num1 = 10;
+var num2 = 20;
+
+function add(num1, num2){
+                console.log("¡Sumando números!");
+                console.log("num1 is: " + num1);
+                console.log("num2 is: " + num2);
+                var sum = num1 + num2;
+                console.log(sum);
+            }
+/*
+
+*/
+
+//el console.log imprime por consola 30
