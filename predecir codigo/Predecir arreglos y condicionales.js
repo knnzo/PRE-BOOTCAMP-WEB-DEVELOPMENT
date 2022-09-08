bef32aee34af36f7f6f1e736b2b8d7873d2c6b51
knnzo <vicente.alcantara.romero@alumnos.sip.cl>
2022-09-08 09:35:46 -0400

    prediccion de codigo.

--- predecir codigo/Predecir arreglos y condicionales.js
+++ predecir codigo/Predecir arreglos y condicionales.js
@@ -3,22 +3,34 @@ function muestraInformaciónContacto() {
     var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
     console.log(auntContactInfo);
 }
+//esta funcion al momento de llmar, muetra la informacion del contacto.
+
 
 
 //Fragmento de código 2
-
-
 function muestraListaDeCompras() {
+
     var produce = ["manzanas", "naranjas"];
+
     var frozen = ["brócoli", "helado"];
+
     frozen.push("croqueta de papa");
+
     console.log(frozen);
 }
+
+//esta funcion agrego "croqueta de papas" al arreglo FROZEN.
+//luego muestra el arreglo.
+
+
 //Fragmento de código 3
-
-
-
 var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+
 movieLibrary.push("Zoro");
+
 movieLibrary[1] = "Beetlejuice";
 console.log(movieLibrary);
+
+//este fragmento agrega zoro al arreglo "movieLibrary"
+//luego reemplaza el indice [1] por "Beetlejuice".
+
